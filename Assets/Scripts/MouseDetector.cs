9774b5f7791b7ba9cb92e076834347332faa602c
felipebassetto-debug <felipe.bassetto@uscsonline.com.br>
2025-10-20 15:11:17 -0300

    pop ups loja

--- Assets/Scripts/MouseDetector.cs
+++ Assets/Scripts/MouseDetector.cs
@@ -4,14 +4,14 @@ using UnityEngine.EventSystems;
 public class MouseDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private SoundManager soundManager;
-    
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("foi");
         if (CompareTag("Button"))
         {
             soundManager.SoundPlay(2);
         }
+        
     }
 
     public void OnPointerExit(PointerEventData eventData) // Caso Necessário
