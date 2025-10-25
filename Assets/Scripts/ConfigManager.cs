b711f27b895e77e69cfa6c1191c2e5490e81d38d
felipebassetto-debug <felipe.bassetto@uscsonline.com.br>
2025-10-25 01:51:54 -0300

    Options

--- Assets/Scripts/ConfigManager.cs
+++ Assets/Scripts/ConfigManager.cs
@@ -1,23 +1,38 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using static UnityEngine.Rendering.STP;
 
 public class ConfigManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public Slider musicSlider;
+    public Slider soundSlider;
+    public Toggle fullScreen;
+
+    public GameDb dB;
+    private float volumeMusicaAtual;
+    private float volumeEfeitosAtual;
+    public Configuracoes configs;
 
     void Start()
     {
-        float volume;
-        if (audioMixer.GetFloat("MusicVolume", out volume))
-        {
-            musicSlider.value = Mathf.Pow(10, volume / 20f);
-        }
+        configs = dB.CarregarConfiguracoes();
+
+        volumeMusicaAtual = configs.VolumeMusica;
+        volumeEfeitosAtual = configs.VolumeEfeitos;
+
+        musicSlider.value = volumeMusicaAtual;
+        soundSlider.value = volumeEfeitosAtual;
+
+        SetMusicVolume(volumeMusicaAtual);
+        SetSoundVolume(volumeEfeitosAtual);
+        
     }
 
     public void SetMusicVolume(float volume)
-   {
+    {
+        volumeMusicaAtual = volume;
         if (volume <= 0.0001f)
         {
         audioMixer.SetFloat("MusicVolume", -80f); // Mudo
@@ -26,6 +41,34 @@ public class ConfigManager : MonoBehaviour
         {
         audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20f);
         }
+    }
+
+    public void SetSoundVolume(float volume)
+    {
+        volumeEfeitosAtual = volume;
+        if (volume <= 0.0001f)
+        {
+            audioMixer.SetFloat("SoundVolume", -80f); // Mudo
+        }
+        else
+        {
+            audioMixer.SetFloat("SoundVolume", Mathf.Log10(volume) * 20f);
+        }
+    }
+
+    public void SaveChanges()
+    {
+        if (fullScreen.isOn)
+        {
+            Screen.fullScreen = true;
+        }
+
+        float volumeMusica = musicSlider.value;
+        float volumeEfeitos = soundSlider.value;
+        string resolucao = configs.Resolucao;
+        bool telaCheia = fullScreen.isOn;
+
+        dB.AtualizarConfiguracoes(configs.Id, volumeMusica, volumeEfeitos, resolucao, telaCheia);
     }
 
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
@@ -11,12 +12,12 @@ public class SoundManager : MonoBehaviour
     // Definição de variaveis audio
     private AudioClip sound;
     public AudioSource audioSource;
+    public AudioMixer audioMixer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        //sound = audioSource.clip;
     }
 
     // Update is called once per frame
@@ -28,6 +29,8 @@ public class SoundManager : MonoBehaviour
     public void SoundPlay(int index)
     {
         sound = listSoundEffects[index];
+        audioMixer.GetFloat("SoundVolume", out float effectsVolume);
+        Debug.Log(effectsVolume);
         audioSource.PlayOneShot(sound, 0.5f); 
     }
 }
