07b7d834d004aadd82e90f94bddf72f7296a83af
felipebassetto-debug <felipe.bassetto@uscsonline.com.br>
2025-11-13 14:20:22 -0300

    ajuste background

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -7,7 +7,6 @@ public class Background : MonoBehaviour
 
     public SpriteRenderer spriteMain;
     private Player player;
-    private Animator anim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,28 +15,23 @@ public class Background : MonoBehaviour
         {
             player = FindFirstObjectByType<Player>();
         }
-        anim = GetComponent<Animator>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        gameObject.GetComponent<SpriteRenderer>().color = spriteMain.color;
+
         if (!player.dead)
         {
-            gameObject.GetComponent<SpriteRenderer>().color = spriteMain.color;
             transform.Translate(Vector2.left * velocidade * Time.deltaTime);
             float positionBackgroundX = transform.position.x;
 
-            if(positionBackgroundX <= -28f)
+            if (positionBackgroundX <= -28f)
             {
                 transform.position = new Vector3(28f, 0, 0);
             }
-        }
-        else
-        {
-            anim.enabled = false;
-        }
+        }   
         
     }
 
